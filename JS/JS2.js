0646d5f0317d23b063203991234aeaaf2f9be19a
GenesisGR <nenegaro@gmail.com>
2024-07-04 19:19:13 -0600

    feat practica 1 clase 04.07

---
+++ JS/JS2.js
@@ -0,0 +1,25 @@
+let paises = ["CR","USA","PA"]
+//3 ELEMENTOS EN EL ARRAY
+
+console.log(paises)
+
+let Estudiantes ={
+    Nombre: "Genesis",
+    Edad: 25,
+    EstadoCivil:true,
+    Materias: ["Progra1", "BD", "Calc"]
+
+}
+
+console.log(Estudiantes)
+
+
+
+let Auto ={
+    Marca: "BYD",
+    Modelo: 3,
+    Interior : ["Asientos", "Freno de Mano", "Cinturon", "Ventana"],
+    Gasolina: true
+}
+
+console.log (Auto)
