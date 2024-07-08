c001ed7b5b7f6d8c158e9fcf72b0dc5fab7578a9
GenesisGR <nenegaro@gmail.com>
2024-07-08 16:52:05 -0600

    feat - tarea 1 JS

--- JS/JS2.js
+++ JS/JS2.js
@@ -1,25 +1,50 @@
 let paises = ["CR","USA","PA"]
 //3 ELEMENTOS EN EL ARRAY
 
-console.log(paises)
+// console.log(paises)
 
-let Estudiantes ={
-    Nombre: "Genesis",
-    Edad: 25,
-    EstadoCivil:true,
-    Materias: ["Progra1", "BD", "Calc"]
+// let Estudiantes ={
+//     Nombre: "Genesis",
+//     Edad: 25,
+//     EstadoCivil:true,
+//     Materias: ["Progra1", "BD", "Calc"]
 
-}
+// }
 
-console.log(Estudiantes)
+//console.log(mensaje)
+
+// let mensaje = "la marca del carro es "+Auto.Marca+ "y el modelo es "+Auto.Modelo+ " por dentro tiene "+Auto.Interior+ " y usa gasolina "+Auto.Gasolina
+// let Auto ={
+//     Marca: "BYD",
+//     Modelo: 3,
+//     Interior : ["Asientos", "Freno de Mano", "Cinturon", "Ventana"],
+//     Gasolina: true
+// }
+
+// console.table (Auto)
+
+//operadores aritmetricos
+
+// let suma =5+3
+// let resta =
+
+//comparacion 
+//console.log(5 == "5")
 
 
+// let N = 25
 
-let Auto ={
-    Marca: "BYD",
-    Modelo: 3,
-    Interior : ["Asientos", "Freno de Mano", "Cinturon", "Ventana"],
-    Gasolina: true
-}
+// if (N >=18)
+//     {alert("si es mayor de edad")
 
-console.log (Auto)
+
+// }
+
+// else{
+//     alert("no es mayor de edad")
+// }
+
+const nombre = "Paposhito";
+if(nombre == "Pedro"){
+    console.log("respuesta"+nombre)
+}// solo si se cumple se muestra el mensaje 
